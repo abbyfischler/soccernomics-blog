cde6583dc44f6ce4d745f5a099b98e633e616037
Abby Fischler <46800781+abbyfischler@users.noreply.github.com>
2022-12-07 11:03:35 -0800

    Updates!

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 
-import NewsletterForm from '@/components/NewsletterForm'
+
 
 const MAX_DISPLAY = 5
 
@@ -89,11 +89,6 @@ export default function Home({ posts }) {
           >
             All Posts &rarr;
           </Link>
-        </div>
-      )}
-      {siteMetadata.newsletter.provider !== '' && (
-        <div className="flex items-center justify-center pt-4">
-          <NewsletterForm />
         </div>
       )}
     </>
