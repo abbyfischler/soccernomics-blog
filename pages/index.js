5559816d97823ebe36476d3375974858427ac1cd
Abby Fischler <46800781+abbyfischler@users.noreply.github.com>
2022-12-12 10:57:44 -0800

    Deleted space

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,6 @@ import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 
-
-
 const MAX_DISPLAY = 5
 
 export async function getStaticProps() {
